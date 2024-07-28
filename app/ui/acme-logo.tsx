29eba8a18b1c4a29da66ea657e6038c9774e9937
agatha-chituwa <bsc-com-06-18@cc.ac.mw>
2024-07-28 11:09:12 +0200

    streaming adding loading skeloton

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,6 +1,7 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
-
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
+// psql "postgres://default:wrfax7NOdX9n@ep-young-field-a4r8hsb2.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+// psql "postgres://default:wrfax7NOdX9n@ep-young-field-a4r8hsb2.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
 export default function AcmeLogo() {
   return (
     <div
